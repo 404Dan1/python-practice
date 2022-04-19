053a7e9a86f94433309c5c64ff9056d10f7aa327
404Dan <94033250+404Dan1@users.noreply.github.com>
2022-04-19 21:06:34 +0000

    Update number-analysis.py

--- number-analysis.py
+++ number-analysis.py
@@ -1,4 +1,4 @@
-
+# NUMBER ANALYSIS
 # Design a program that asks the user to enter a series of 20 numbers. The program should store the numbers
 #   in a list then display the following data:
 #     + The lowest number in the list

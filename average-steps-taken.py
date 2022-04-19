42c1b83c4bc0169e8e83692c816bf9ea55414173
404Dan <94033250+404Dan1@users.noreply.github.com>
2022-04-19 21:05:36 +0000

    Update average-steps-taken.py

--- average-steps-taken.py
+++ average-steps-taken.py
@@ -6,7 +6,18 @@
 month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 months = [
-  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+  "January", 
+  "February", 
+  "March", 
+  "April", 
+  "May", 
+  "June", 
+  "July", 
+  "August", 
+  "September", 
+  "October", 
+  "November", 
+  "December"
 ]
 
 first_month = 0
